1578bf47635b3b0bdde8e27604b483fc3feb0da7
Hania <hania_nina@live.fr>
2022-10-08 16:56:32 +0200

    ipad Media Query

--- script.js
+++ script.js
@@ -30,10 +30,17 @@ const changeSlide = (direction) => {
 /* Navbar button */
 
 const bar = document.getElementById('bar');
+const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
     })
 }
